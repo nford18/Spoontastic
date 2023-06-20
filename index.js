04511104adbe431d5790b1581b144dc8c6224644
Jesse Leonard <leonajes@mail.gvsu.edu>
2023-06-19 23:41:53 -0400

    Merged index files

--- index.js
+++ index.js
@@ -6,6 +6,47 @@ const GitHubStrategy = require('passport-github2').Strategy;
 const MongoDBStore = require('connect-mongodb-session')(session);
 const app = express();
 const json = require('json')
+const fs = require('fs');
+const csv = require('csv-parser');
+
+let questions = [];
+let currentQuestion;
+
+function readCSVFile() {
+	return new Promise((resolve, reject) => {
+	  const results = [];
+  
+	  fs.createReadStream('GeneralKnowledge.csv')
+		.pipe(csv())
+		.on('data', (data) => results.push(data))
+		.on('end', () => {
+		  console.log('CSV file successfully processed.');
+		  console.log(results);
+		  resolve(results);
+		})
+		.on('error', reject);
+	});
+  }
+  
+  async function populateQuestions() {
+	try {
+	  const csvData = await readCSVFile();
+	  questions = csvData.map((data) => ({
+		question: data['question'],
+		answer: data['answer']
+	  }));
+	  console.log('Questions populated:', questions);
+	  currentQuestion = getRandomQuestion();
+	  console.log('Current question:', currentQuestion);
+	} catch (error) {
+	  console.error('Error processing CSV file:', error);
+	}
+  }
+  
+  function getRandomQuestion() {
+	const randomIndex = Math.floor(Math.random() * questions.length);
+	return questions[randomIndex];
+  }
 
 app.use(express.static('public'));
 
@@ -83,7 +124,43 @@ app.get('/verify',
 		console.log(err);
 	}
     });
-    res.redirect('/');
+    res.redirect('/question');
+  });
+
+app.post('/answer', express.urlencoded({ extended: true }), (req, res) => {
+	const userAnswer = req.body.userAnswer;
+	const correctAnswer = currentQuestion.answer;
+  
+	if (userAnswer === correctAnswer) {
+	  res.redirect('/');
+	} else {
+	  currentQuestion = getRandomQuestion();
+	  const questionText = currentQuestion.question;
+	  res.send(`
+		<h1>${questionText}</h1>
+		<form action="/answer" method="POST">
+		  <input type="text" name="userAnswer" required>
+		  <button type="submit">Submit</button>
+		</form>
+		<p>Incorrect answer. Try again.</p>
+	  `);
+	}
+});
+
+app.get('/questions', (req, res) => {
+	if (questions.length === 0) {
+	  res.send('No questions available.');
+	  return;
+	}
+  
+	const questionText = currentQuestion.question;
+	res.send(`
+	  <h1>${questionText}</h1>
+	  <form action="/answer" method="POST">
+		<input type="text" name="userAnswer" required>
+		<button type="submit">Submit</button>
+	  </form>
+	`);
   });
 
 app.get('/logout', (req, res) => {
@@ -92,7 +169,10 @@ app.get('/logout', (req, res) => {
 	});
 });
 
-app.listen(port, async () => { 
-	await mongoose.connect(uri);
-	console.log("Up and running.");
+populateQuestions().then(() => {
+	app.listen(port, async () => { 
+		await mongoose.connect(uri);
+		console.log("Up and running.");
+	});
 });
+
